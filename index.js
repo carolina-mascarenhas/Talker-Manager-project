04a9610b3ac02225e25e59b378a87dd548d0dbc4
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-05-20 19:17:15 -0300

    validacao de email e senha de POST /login

--- index.js
+++ index.js
@@ -9,6 +9,34 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+function validateEmail(req, res, next) {
+  const { email } = req.body;
+
+  if (!email || email === '') {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!email.match(/\S+@\S+\.\S+/)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  next();
+}
+
+function validatePassword(req, res, next) {
+  const { password } = req.body;
+
+  if (!password || password === '') {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  next();
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -36,7 +64,7 @@ app.get('/talker/:id', (req, res) => {
     });
 });
 
-app.post('/login', (_req, res) => {
+app.post('/login', validateEmail, validatePassword, (_req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   res.status(200).json({ token });
 });
