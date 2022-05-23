c66a1a5b7bad6229d3fe938ca445c53dfbd8f417
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-05-23 12:10:52 -0300

    POST /talker em andamento

--- routes/talker.js
+++ routes/talker.js
@@ -3,6 +3,32 @@ const fs = require('fs').promises;
 
 const routes = express.Router();
 
+const checkName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const checkAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
 routes.get('/', (_req, res) => {
   fs.readFile('./talker.json', 'utf-8')
     .then((fileContent) => {
@@ -23,6 +49,20 @@ routes.get('/:id', (req, res) => {
       }
       res.status(200).json(findById);
     });
+});
+
+routes.post('/', checkName, checkAge, async (req, res) => {
+  const { name, age, talk: { watchedAt, rate } } = req.body;
+  const contentFile = await fs.readFile('./talker.json', 'utf-8');
+  const arr = JSON.parse(contentFile);
+  arr.push({
+    name,
+    age,
+    talk: { watchedAt, rate },
+  });
+  console.log(arr);
+  await fs.writeFile('./talker.json', JSON.stringify(arr));
+  res.status(201).json({ name, age, talk: { watchedAt, rate } });
 });
 
 module.exports = routes;
